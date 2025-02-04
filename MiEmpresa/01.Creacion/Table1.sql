ce684dd3712af5cd55fbc2e01a409d16fa7dda95
Leonel Esteban Garcia Figueroa <L23170044@culiacan.tecnm.mx>
2025-02-04 18:00:43 +0000

    Dia 4

--- MiEmpresa/01.Creacion/Table1.sql
+++ MiEmpresa/01.Creacion/Table1.sql
@@ -1,4 +1,12 @@
 CREATE TABLE [dbo].[Table1]
 (
-  [Id] INT NOT NULL PRIMARY KEY
+  [Id] INT NOT NULL PRIMARY KEY,
+
+  firstname NVARCHAR(50),
+  lastname NVARCHAR (50)
 )
+
+select * from table1;
+
+insert into table1 )id, firstname, lastname)
+values (1, 'Emilio'), 
